472a346d647facaba9a078f170a3e55b1cf85979
Philipp Rosner <philipp.rosner@tum.de>
2023-11-09 09:29:13 +0100

    Consolidated input data

--- battery.py
+++ battery.py
@@ -4,8 +4,6 @@ import pickle
 import rainflow
 import scipy.interpolate as spip
 
-from dateutil.relativedelta import relativedelta
-
 class BatteryPack:
 
     def __init__(self, scenario, commodity):
@@ -18,13 +16,13 @@ class BatteryPack:
         self.soc_max = 1  # Upper SOC limit
         self.soc_min = 0  # Lower SOC limit
 
-        self.c_cell = 76.27  # Cell heat capacity as per Forgez et al. # todo find unit
-        self.c_aluminium = 896  # Specific heat capacity of the pack housing (made from Al) in J/(kg K)
-        self.r_th_ch = 3.3  # Thermal resistance between cell and pack housing # todo find unit
-        self.r_th_ha = 4.343  # Thermal resistance between pack housing and ambient in W/K [Neubauer et al.]
-        # self.k_bh = 0.899  # K/W #OlafDiss
-        # self.k_out = 10.9  # W/K OlafDiss
+        # Thermal model parameters
+        self.c_th_spec_housing = 896  # Specific heat capacity of the pack housing (made from Al) in J/(kg K)
+        self.c_th_spec_cell = 1045  # Specific heat capacity of LI cells as per Teichert's dissertation in J/(kg K)
+        self.k_c2h = 0.899  # Thermal conductance between cell and housing as per Teichert's dissertation in W/K
+        self.k_h2a = 10.9  # Thermal conductance between housing and ambient as per Teichert's dissertation in W/K
 
+        # Active thermal control system parameters
         # self.p_cool = 10e3  # System cooling power in W [Schimpe et al.]
         # self.p_heat = 11.2e3  # System heating power in W [Schimpe et al.]
         # self.cop_cool = -3  # Coefficient of performance of cooling system in pu [Schimpe et al.]
@@ -33,9 +31,6 @@ class BatteryPack:
         # self.t_cool_off = 30  # Cooling deactivation threshold in °C
         # self.t_heat_on = 10  # Heating activation threshold in °C
         # self.t_heat_off = 15 # Heating deactivation threshold in °C
-
-        # self.e_spec_grav_c2p = [0.59, 0.71]
-        # self.e_spec_vol_c2p = [0.39, 0.55]
 
         # Initial values for aging state tracking
         self.q_loss_cal = np.zeros(scenario.nhorizons)
@@ -49,25 +44,33 @@ class BatteryPack:
         if self.chemistry == 'nmc':
             # Cell from Schmalstieg et al. - Sanyo UR18650E
             self.q_nom_cell = 2.15  # Typical capacity in Ah
-            self.u_nom = 3.6  # Nominal voltage in V
-            self.u_min = 3.0  # Minimum voltage in V
-            self.u_max = 4.2  # Maximum voltage in V
-            self.i_max_cont = 2.05  # Maximum charging current in A
-            self.i_min_cont = -6.15  # Maximum discharging current in A
-            self.m = 0.0445  # Cell mass in kg
-            self.v = 0.0165  #Cell volume in L
+            self.u_nom_cell = 3.6  # Nominal voltage in V
+            self.u_min_cell = 3.0  # Minimum voltage in V
+            self.u_max_cell = 4.2  # Maximum voltage in V
+            self.i_max_cont_cell = 2.05  # Maximum charging current in A
+            self.i_min_cont_cell = -6.15  # Maximum discharging current in A
+            self.m_cell = 0.0445  # Cell mass in kg
+            self.v_cell = 0.0165  #Cell volume in L
+
+            self.e_spec_grav_c2p = 0.59  # Transformation factor of gravimetric energy density from cell to pack level
+            self.e_spec_vol_c2p = 0.39  # Transformation factor of volumetric energy density from cell to pack level
+
             self.data_path = os.path.join(os.getcwd(), 'input', 'battery', 'sanyo_ur18650e.pkl')
 
         elif self.chemistry == 'lfp':
             # Cell from Naumann et al. - Sony US26650
             self.q_nom_cell = 3.0  # Typical capacity in Ah
-            self.u_nom = 3.2  # Nominal voltage in V
-            self.u_min = 2.0  # Minimum voltage in V
-            self.u_max = 3.6  # Maximum voltage in V
-            self.i_max_cont = 3  # Maximum charging current in A
-            self.i_min_cont = -20  # Maximum discharging current in A
+            self.u_nom_cell = 3.2  # Nominal voltage in V
+            self.u_min_cell = 2.0  # Minimum voltage in V
+            self.u_max_cell = 3.6  # Maximum voltage in V
+            self.i_max_cont_cell = 3  # Maximum charging current in A
+            self.i_min_cont_cell = -20  # Maximum discharging current in A
             self.m_cell = 0.0845  # Cell mass in kg
             self.v_cell = 0.0345  #Cell volume in L
+
+            self.e_spec_grav_c2p = 0.71  # Transformation factor of gravimetric energy density from cell to pack level
+            self.e_spec_vol_c2p = 0.55  # Transformation factor of volumetric energy density from cell to pack level
+
             self.data_path = os.path.join(os.getcwd(), 'input', 'battery', 'sony_us26650.pkl')
 
         with open(self.data_path, 'rb') as file:
@@ -88,6 +91,7 @@ class BatteryPack:
         self.e_cell = self.q_nom_cell * self.u_nom  # Nominal energy content of the cell in Wh
         self.e_spec_grav_cell = self.e_cell / self.m_cell
         self.e_spec_vol_cell = self.e_cell / self.v_cell
+        self.c_th_cell = self.m_cell * self.c_th_spec_cell
 
         if self.parent.parent.opt:  # self.parent.parent is CommoditySystem
             self.size = None  # placeholder for nominal pack capacity - to be filled after size optimization
@@ -193,12 +197,13 @@ class BatteryPack:
             stress_factor = np.mean(stress_factor)
 
         # Calculate previous aging state as equivalent time at current conditions
-        t_eq_Q = (np.sum(self.q_loss_cal) / (k_tuning * alpha_cap)) ** (4 / 3)
-        t_eq_R = (np.sum(self.r_inc_cal) / (k_tuning * alpha_res)) ** (4 / 3)
+        t_eq_q = (np.sum(self.q_loss_cal) / (k_tuning * alpha_cap)) ** (4 / 3)
+        t_eq_r = (np.sum(self.r_inc_cal) / (k_tuning * alpha_res)) ** (4 / 3)
 
         # Calculate calendric aging
-        self.q_loss_cal[horizon.index] = k_tuning * alpha_cap * ((t_eq_Q + t_aging) ** 0.75)
-        self.r_inc_cal[horizon.index] = k_tuning * alpha_cap * ((t_eq_R + t_aging) ** 0.75)
+        t_hor_days = self.t_hor / (3600 * 24)  # Schmalstieg model is evaluated in days
+        self.q_loss_cal[horizon.index] = k_tuning * alpha_cap * ((t_eq_q + t_hor_days) ** 0.75)
+        self.r_inc_cal[horizon.index] = k_tuning * alpha_cap * ((t_eq_r + t_hor_days) ** 0.75)
 
         # Calculate mean OCV of each detected cycle
         ocv_cycles_mean = self.ocv_interp(self.cycles_hor['mean'])
@@ -212,67 +217,74 @@ class BatteryPack:
         for stress_factor in [beta_cap, beta_res]:
             stress_factor = np.sum(stress_factor * self.dod_hor) / np.sum(self.dod_hor)
 
-        # Q_tot_age = Qtot * 2  # is this conversion to FEC or sth?
-        q_eq = sum(self.q_loss_cyc) / (k_tuning * (beta_cap ** 2))  # todo check against paper
+        # Define previous aging state as equivalent FECs at current conditions
+        q_eq = (sum(self.q_loss_cyc) / (k_tuning * beta_cap)) ** 2
 
         # Calculate cyclic aging
-        self.q_loss_cyc[horizon.index] = k_tuning * beta_cap * ((Q_tot_eq + Q_tot_age) ** 0.5)
-        self.r_inc_cal[horizon.index] = k_tuning * beta_res * Q_tot_age
+        self.q_loss_cyc[horizon.index] = k_tuning * beta_cap * ((q_eq + self.q_tot_hor) ** 0.5)
+        self.r_inc_cal[horizon.index] = k_tuning * beta_res * self.q_tot_hor
 
 
     def rint_model(self, p_out):
+        """
+        This function calculates output voltage and current of a battery cell based on a simple Zero-RC Equivalent
+        Circuit Model consisting of an ideal voltage source and a series resistance. Power loss to heat at the series
+        resistor can also be evaluated
+        """
 
         ocv = self.ocv_func(self.soc)
+
+        # Charge power is positive, Discharge is negative
         if p_out > 0:
             r_i = self.r_i_ch_func(self.t_cell[-1], self.soc[-1])
-        else:
-            r_i_dch = self.r_i_dch_func(self.t_cell[-1], self.soc[-1])
+        else:  # Case p_out = 0 is irrelevant, as current is 0 anyway
+            r_i = self.r_i_dch_func(self.t_cell[-1], self.soc[-1])
 
         i = np.real((-ocv + np.sqrt((ocv ** 2) + (4 * r_i * p_out))) / (2 * r_i))
-        p_loss = (i ** 2) * r_i
+        p_loss = r_i * (i ** 2)
 
         return i, p_loss
 
-    def thermal_model(bet, cell, n_cells, T_Cell, T_Housing, T_amb, P_Loss, Pcool, Pheat, dt):
+    # def thermal_model(self,bet, cell, n_cells, T_Cell, T_Housing, T_amb, P_Loss, Pcool, Pheat, dt):
+    #
+    #     # Mass Calculation based on battery size
+    #     m_housing = ((n_cells * cell.Qnom * cell.Unom) / cell.grav_energy_density) * (1 - cell.c2p_grav)
+    #
+    #
+    #     c_th_housing = self.c_th_spec_housing * m_housing  # J/K
+    #
+    #     T_Housing_new = T_Housing + dt * (
+    #                 ((Pcool * bet.COPcool) + (Pheat * bet.COPheat) + bet.k_bh * n_cells * (T_Cell -
+    #                                                                                        T_Housing) + bet.k_out * (
+    #                              T_amb - T_Housing)) / Cth_Housing)
+    #
+    #     T_Cell_new = T_Cell + dt * ((P_Loss + bet.k_bh * (T_Housing - T_Cell)) / Cth_Battery)
+    #
+    #     return T_Cell_new, T_Housing_new
 
-        # Mass Calculation based on battery size
-        m_battery_wo_cells = ((n_cells * cell.Qnom * cell.Unom) / cell.grav_energy_density) * (1 - cell.c2p_grav)
-        Cth_Battery = cell.mass * 1045  # J/(kgK) OlafDiss
-
-        Cth_Housing = bet.c_aluminium * m_battery_wo_cells  # J/K
-
-        T_Housing_new = T_Housing + dt * (
-                    ((Pcool * bet.COPcool) + (Pheat * bet.COPheat) + bet.k_bh * n_cells * (T_Cell -
-                                                                                           T_Housing) + bet.k_out * (
-                                 T_amb - T_Housing)) / Cth_Housing)
-
-        T_Cell_new = T_Cell + dt * ((P_Loss + bet.k_bh * (T_Housing - T_Cell)) / Cth_Battery)
-
-        return T_Cell_new, T_Housing_new
-
-    def thermal_control(bet, T_Cell, p_cool_prev, p_value_control, p_value, n_cells):
-
-        # Control Algorithm for active Cooling
-        if T_Cell < bet.T_Heat:
-            P_Heat = bet.Pheater
-            P_Cool = 0
-        elif T_Cell > bet.T_Cool_on or (
-                T_Cell > bet.T_Cool_off and p_cool_prev > 0):  # Cool if Cooling-Threshold is exeeded or (if Cooling was active in the previous time step and Off-Threshold is not reached yet)
-            P_Heat = 0
-            P_Cool = bet.Pcooler
-        else:
-            P_Heat = 0
-            P_Cool = 0
-
-        # Impact of Cooling on Power
-
-        # Case Driving // Cooling Power added to Power demand of driving task
-        if p_value <= 0:
-            p_value_control = p_value_control + (P_Cool + P_Heat) / n_cells
-
-        # Case Charging // Cooling Power from Infrastructure -> If Cell is limiting no further power demand from cooling
-        else:
-            if p_value >= p_value_control:
-                p_value_control = p_value_control - (P_Cool + P_Heat) / n_cells
-
-        return p_value_control, P_Cool, P_Heat
+    # def thermal_control(bet, T_Cell, p_cool_prev, p_value_control, p_value, n_cells):
+    #
+    #     # Control Algorithm for active Cooling
+    #     if T_Cell < bet.T_Heat:
+    #         P_Heat = bet.Pheater
+    #         P_Cool = 0
+    #     elif T_Cell > bet.T_Cool_on or (
+    #             T_Cell > bet.T_Cool_off and p_cool_prev > 0):  # Cool if Cooling-Threshold is exeeded or (if Cooling was active in the previous time step and Off-Threshold is not reached yet)
+    #         P_Heat = 0
+    #         P_Cool = bet.Pcooler
+    #     else:
+    #         P_Heat = 0
+    #         P_Cool = 0
+    #
+    #     # Impact of Cooling on Power
+    #
+    #     # Case Driving // Cooling Power added to Power demand of driving task
+    #     if p_value <= 0:
+    #         p_value_control = p_value_control + (P_Cool + P_Heat) / n_cells
+    #
+    #     # Case Charging // Cooling Power from Infrastructure -> If Cell is limiting no further power demand from cooling
+    #     else:
+    #         if p_value >= p_value_control:
+    #             p_value_control = p_value_control - (P_Cool + P_Heat) / n_cells
+    #
+    #     return p_value_control, P_Cool, P_Heat
