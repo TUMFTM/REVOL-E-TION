b14f2003b1d80edb0a50907fa795537c99bca231
Florian Honeder <florian@MacBook-Air-von-Florian.local>
2024-02-23 11:03:26 +0100

    Update stopping condition in epf.py

--- epf.py
+++ epf.py
@@ -19,31 +19,37 @@ class EPF:
         elif self.model == 'dnn':
             self.data = self.data[['Real', 'dnn']]
             pass
-        #elif self.model == 'real':
-            #pass
+
 
     def update_costs(self, ph_dti, costs):
         # ToDo: update cost series for the ph_dti time index with the real day ahead prices
         #  for day X+1 and the predicted prices for day X+2
 
-        self.costs = costs
         start_date = ph_dti[0]
 
         day1_range = pd.date_range(start_date, start_date + pd.Timedelta(days=1)- pd.Timedelta(minutes=15), freq='15T')
         day2_range = pd.date_range(start_date + pd.Timedelta(days=1), start_date + pd.Timedelta(days=2) - pd.Timedelta(minutes=15), freq='15T')
+
+        day1_range = day1_range[day1_range.isin(costs.index)]
+        day2_range = day2_range[day2_range.isin(costs.index)]
+
         costs.loc[day1_range] = self.data.loc[day1_range, 'Real']
         costs.loc[day2_range] = self.data.loc[day2_range, self.model]
 
         # you can specify the name of the models here. Just make sure to use the same name in the sgen file
+        #letzter tag von day 2 range muss noch in kosten vektor sein
 
         return costs
 
-    def get_flow(self, flow_in, flow_out):
+    def get_flow(self, flow_in, flow_out, scenario):
         # flow_in = flow from grid to local grid
         # flow_out = flow from local grid to grid
         # ToDo: Use these flows to calculate the costs or revenues related to the energy flow from and to the grid
         #  print or save the results
+
         result = self.costs * (flow_in + flow_out)
+        #self.data gehen und diese Daten verwenden
+        #sim_dti Parameter in scenario - beinhaltet gesamten Zeitraum der simulation
         result = result.sum()
         print(result)
 
