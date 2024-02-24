c5b784753327b08c0d22ff9901dddc4ed6fcaf5c
Florian Honeder <florian@MacBook-Air-von-Florian.local>
2024-02-24 10:29:32 +0100

    Update summing up costs in epf.py

--- epf.py
+++ epf.py
@@ -10,8 +10,8 @@ class EPF:
         path = './input/grid/'
         dataset = 'ALL'
         file_path = os.path.join(path, dataset + '.csv')
-        self.data = pd.read_csv(file_path, index_col=0, parse_dates=True)
-        self.data.columns = ['Real', 'statistical', 'dnn']
+        self.data = pd.read_csv(file_path, index_col=0, parse_dates=True, float_precision='round_trip')
+        self.data.columns = ['Real', 'statistical', 'dnn', 'actual']
 
         if self.model == 'statistical':
             self.data = self.data[['Real', 'statistical']]
@@ -19,7 +19,9 @@ class EPF:
         elif self.model == 'dnn':
             self.data = self.data[['Real', 'dnn']]
             pass
-
+        elif self.model == 'actual':
+            self.data = self.data[['Real', 'actual']]
+            pass
 
     def update_costs(self, ph_dti, costs):
         # ToDo: update cost series for the ph_dti time index with the real day ahead prices
@@ -46,11 +48,36 @@ class EPF:
         # flow_out = flow from local grid to grid
         # ToDo: Use these flows to calculate the costs or revenues related to the energy flow from and to the grid
         #  print or save the results
+        overall_result = pd.DataFrame(index=scenario.sim_dti)
 
-        result = self.costs * (flow_in + flow_out)
+        if self.model == 'statistical':
+            self.data = self.data[['statistical']]
+            result_statistical = self.data.loc[scenario.sim_dti] * (1/1000000)*(flow_in + flow_out)
+            result_statistical = result_statistical.sum()
+            print(result_statistical)
+            overall_result['result_statistical'] = result_statistical
+            pass
+        elif self.model == 'dnn':
+            self.data = self.data[['dnn']]
+            result_dnn = self.data.loc[scenario.sim_dti] *(1/1000000)* (flow_in + flow_out)
+            result_dnn = result_dnn.sum()
+            print(result_dnn)
+            overall_result['result_dnn'] = result_dnn
+            pass
+        elif self.model == 'actual':
+            self.data = self.data[['actual']]
+            result_actual = self.data.loc[scenario.sim_dti]*(1/1000000)* (flow_in + flow_out)
+            result_actual = result_actual.sum()
+            print(result_actual)
+            overall_result['result_actual'] = result_actual
+            pass
+
+        path_result = './results/'
+        if not os.path.exists(path_result):
+            os.makedirs(path_result)
+        overall_result.to_csv(os.path.join(path_result,'overall_result.csv'))
+
         #self.data gehen und diese Daten verwenden
         #sim_dti Parameter in scenario - beinhaltet gesamten Zeitraum der simulation
-        result = result.sum()
-        print(result)
 
-        return result
+        return overall_result
