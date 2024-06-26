97bbd2e495ad8dafdbb2294df4d08807b9525a2b
Philipp Rosner <philipp.rosner@tum.de>
2024-06-26 20:46:48 +0200

    Redid LCOE calculation, introduced lcoe_wocs excluding CommoditySystem expenses

--- simulation.py
+++ simulation.py
@@ -427,14 +427,15 @@ class Scenario:
             except ZeroDivisionError:
                 run.logger.warning(f'Scenario {self.name} - renewable share calculation: division by zero')
 
-        if self.e_dis_del == 0 or self.e_prj_del == 0:
+        totex_dis_cs = sum([cs.totex_dis for cs in self.commodity_systems.values()])
+        if self.e_dis_del == 0:
             run.logger.warning(f'Scenario {self.name} - LCOE calculation: division by zero')
         else:
             try:
-                self.lcoe = self.totex_dis / self.e_prj_del
-                self.lcoe_dis = self.totex_dis / self.e_dis_del
+                self.lcoe = self.totex_dis / self.e_dis_del
+                self.lcoe_wocs = (self.totex_dis - totex_dis_cs) / self.e_dis_del
             except ZeroDivisionError:
-                self.lcoe = self.lcoe_dis = None
+                self.lcoe = self.lcoe_wocs = None
                 run.logger.warning(f'Scenario {self.name} - LCOE calculation: division by zero')
 
         self.npv = self.crev_dis - self.totex_dis
@@ -443,12 +444,12 @@ class Scenario:
 
         # print basic results
         run.logger.info(f'Scenario \"{self.name}\" -'
+                        f' NPC {round(self.totex_dis):,} {self.currency} -'
                         f' NPV {round(self.npv):,} {self.currency} -'
-                        f' LCOE {round(self.lcoe_dis * 1e5, 1)} {self.currency}-ct/kWh -'
+                        f' LCOE {round(self.lcoe_wocs * 1e5, 1)} {self.currency}-ct/kWh -'
                         f' mIRR {round(self.mirr * 100, 1)} % -'
                         f' Renewable Share: {round(self.renewable_share * 100, 1)} % -'
-                        f' Renewable Curtailment: {round(self.renewable_curtailment * 100, 1)} % -'
-                        f' Core Efficiency: {round(self.e_eta * 100, 1)} %')
+                        f' Renewable Curtailment: {round(self.renewable_curtailment * 100, 1)} %')
 
     def create_block_objects(self, class_dict, run):
         objects = {}
