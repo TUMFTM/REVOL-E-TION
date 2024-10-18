901358dc2aa0156b9b5c4d72b838b697f25758f8
Philipp Rosner <philipp.rosner@tum.de>
2024-10-18 07:43:30 +0200

    updated PVGIS API limit check to PVGIS 5.3

--- revoletion/blocks.py
+++ revoletion/blocks.py
@@ -1646,12 +1646,12 @@ class PVSource(RenewableInvestBlock):
 
             if self.api_length > 15:
                 raise ValueError('PVGIS API only allows a maximum of 15 years of data')
-            elif self.api_endyear > 2020:  # PVGIS-SARAH2 only has data up to 2020
-                self.api_shift = (pd.to_datetime('2020-01-01 00:00:00+00:00') -
+            elif self.api_endyear > 2023:  # PVGIS-SARAH3 only has data up to 2023
+                self.api_shift = (pd.to_datetime('2023-01-01 00:00:00+00:00') -
                                   pd.to_datetime(f'{self.api_endyear}-01-01 00:00:00+00:00'))
                 self.api_endyear = 2020
                 self.api_startyear = 2020 - self.api_length
-            elif self.api_startyear < 2005:  # PVGIS-SARAH2 only has data from 2005
+            elif self.api_startyear < 2005:  # PVGIS-SARAH3 only has data from 2005
                 self.api_shift = (pd.to_datetime('2005-01-01 00:00:00+00:00') -
                                   pd.to_datetime(f'{self.api_startyear}-01-01 00:00:00+00:00'))
                 self.api_startyear = 2005
