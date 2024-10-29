57e610ba3b3168c4881e4cfa1932f471755d000d
Brian Dietermann <brian.dietermann@tum.de>
2024-10-29 08:04:59 +0100

    Temporary fix for index error in save_result_summary. Doesn't solve the root problem of the error (calling function twice)

--- revoletion/simulation.py
+++ revoletion/simulation.py
@@ -590,8 +590,7 @@ class Scenario:
                         self.result_summary.loc[(block_name, f'power_opex_{interval}'), self.name] = (
                             block_obj.peakshaving_ints.loc[interval, ['period_fraction', 'power', 'opex_spec']].prod())
 
-        self.result_summary.reset_index(inplace=True, names=['block', 'key'])
-        self.result_summary.to_csv(self.path_result_summary_tempfile, index=False)
+        self.result_summary.to_csv(self.path_result_summary_tempfile, index=True)
 
     def save_result_timeseries(self):
         for block in self.blocks.values():
