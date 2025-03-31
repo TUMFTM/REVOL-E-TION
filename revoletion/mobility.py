7a6c598a13c5ddbae7612a09b2841170faf71c8d
Philipp Rosner <philipp.rosner@tum.de>
2025-03-31 10:04:48 +0200

    move read_demand file to mobility.py

--- revoletion/mobility.py
+++ revoletion/mobility.py
@@ -183,7 +183,21 @@ class SubFleetDemand:
             self.demand.to_csv(demand_path)
         # endregion
 
+    def read_demand_file(self):
+        """
+        read in a subfleet demand csv file directly
+        """
+        path_demand_file = os.path.join(self.scenario.run.paths['input'],
+                                        utils.set_extension(self.subfleet.filename))
+        self.demand = pd.read_csv(path_demand_file,
+                         index_col=0)
 
+        self.demand['time_req'] = pd.to_datetime(self.demand['time_req'], utc=True).dt.tz_convert(self.scenario.timezone)
+        self.demand['dtime_active'] = pd.to_timedelta(self.demand['dtime_active'])
+        self.demand['dtime_idle'] = pd.to_timedelta(self.demand['dtime_idle'])
+        self.demand['dtime_patience'] = pd.to_timedelta(self.demand['dtime_patience'])
+
+        self.demand = self.demand.loc[self.demand['time_req'].isin(self.scenario.dti_sim), :]
 
 
 class BatteryDemand(SubFleetDemand):
@@ -210,6 +224,7 @@ class BatteryDemand(SubFleetDemand):
         self.demand['energy_req'] = (self.demand
                                      .groupby(['usecase', 'timeframe'])['energy_req']
                                      .transform(sample_energy_usecase))
+
         def calc_time_active_usecase(group):
             """
             groupby function
