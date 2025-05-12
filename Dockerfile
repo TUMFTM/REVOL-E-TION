0b224cf8dac9fbce5b4b479e24ea99c2ee0d0c90
Jan-Niklas Weghorn <jrester379@gmail.com>
2025-05-12 09:50:36 +0200

    feat: optimize docker image and make tkinter optional
    Reduce docker image size and build time. Include CBC and gurobi linear
    solvers by default. Make tkinter optional, to better support docker environment.

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,60 @@
-FROM python:3.11
+ARG PYTHON_VERSION=3.11-slim
+FROM python:${PYTHON_VERSION} AS builder
 
-# Copy application code
-COPY . /revoletion/
-WORKDIR /
+WORKDIR /app
 
 # Install system dependencies
 RUN apt-get update && \
-    apt-get install -y wget && \
-    rm -rf /var/lib/apt/lists/*
+    apt-get install -y --no-install-recommends wget
 
-# Download and install Gurobi
-RUN wget https://packages.gurobi.com/12.0/gurobi12.0.0_linux64.tar.gz && \
-    tar xvf gurobi12.0.0_linux64.tar.gz && \
-    rm gurobi12.0.0_linux64.tar.gz && \
-    mv gurobi1200 /opt/gurobi
+# Download and install Gurobi.
+ARG GUROBI_MAJOR_MINOR_VERSION=12.0
+ARG GUROBI_PATCH_VERSION=0
+RUN mkdir -p /opt/gurobi && \
+    wget "https://packages.gurobi.com/${GUROBI_MAJOR_MINOR_VERSION}/gurobi${GUROBI_MAJOR_MINOR_VERSION}.${GUROBI_PATCH_VERSION}_linux64.tar.gz" && \
+    tar xvf "gurobi${GUROBI_MAJOR_MINOR_VERSION}.${GUROBI_PATCH_VERSION}_linux64.tar.gz" --directory=/opt/gurobi
 
-# Set environment variables for Gurobi
+# Download and install CBC.
+ARG CBC_VERSION=2.10.12
+# By default use the static version, which makes the dependency management easier.
+ARG CBC_BUILD=x86_64-ubuntu22-gcc1140-static
+RUN mkdir -p /opt/cbc && \
+    wget "https://github.com/coin-or/Cbc/releases/download/releases%2F${CBC_VERSION}/Cbc-releases.${CBC_VERSION}-${CBC_BUILD}.tar.gz" && \
+    tar xvf "Cbc-releases.${CBC_VERSION}-${CBC_BUILD}.tar.gz" --directory=/opt/cbc
+
+# Enable bytecode compilation
+ENV UV_COMPILE_BYTECODE=1
+
+# Copy from the cache instead of linking since it's a mounted volume
+ENV UV_LINK_MODE=copy
+
+ENV UV_SYSTEM_PYTHON=1
+
+RUN --mount=from=ghcr.io/astral-sh/uv,source=/uv,target=/bin/uv \
+    --mount=type=bind,source=pyproject.toml,target=pyproject.toml \
+    --mount=type=cache,target=/root/.cache/uv \
+    uv sync --no-install-project --no-editable
+
+FROM python:${PYTHON_VERSION} AS runtime
+
+# Copy Gurobi from builder stage.
+COPY --from=builder /opt/gurobi /opt/gurobi
+# Add Gurobi to Path, so that REVOL-E-TION can execute it.
 ENV PATH="/opt/gurobi/bin:${PATH}" \
     LD_LIBRARY_PATH="/opt/gurobi/lib:${LD_LIBRARY_PATH:-}"
 
-# Install Python dependencies
-RUN pip install /revoletion/
+# Copy CBC from builder stage.
+COPY --from=builder /opt/cbc /opt/cbc
+# Add CBC to Path, so that REVOL-E-TION can execute it.
+ENV PATH="/opt/cbc/bin:${PATH}" \
+    LD_LIBRARY_PATH="/opt/cbc/lib:${LD_LIBRARY_PATH:-}"
+
+WORKDIR /app
+
+COPY --from=builder /app/.venv /app/.venv
+ENV PATH="/app/.venv/bin:$PATH"
+
+COPY . .
+
+ENTRYPOINT ["python", "-m", "revoletion.main"]
+CMD []
--- revoletion/main.py
+++ revoletion/main.py
@@ -2,9 +2,19 @@
 
 import argparse
 import os
-import tkinter as tk
-import tkinter.filedialog
 import warnings
+
+try:
+    import tkinter as tk
+    import tkinter.filedialog
+
+    TKINTER_AVAILABLE = True
+except ImportError:
+    TKINTER_AVAILABLE = False
+    warnings.warn(
+        "tkinter is not available in this environment. GUI file selection will be disabled."
+    )
+
 
 from revoletion import simulation as sim
 
@@ -14,50 +24,79 @@ class DefaultFileLocationWarning(UserWarning):
 
 
 def main():
-
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-scn', '--scenario',
-                        type=str,
-                        default=None,
-                        help='Path to the scenario CSV file')
-    parser.add_argument('-in', '--inputdir',
-                        type=str,
-                        default=None,
-                        help='Path to the input data directory')
-    parser.add_argument('-out', '--outputdir',
-                        type=str,
-                        default=None,
-                        help='Path to the results directory')
-    parser.add_argument('-slv', '--solver',
-                        type=str,
-                        default='gurobi',
-                        help='Pyomo compatible solver to be used for the optimization problem.')
-    parser.add_argument('-np', '--n_processes',
-                        type=int,
-                        default=1,
-                        help='Number of processes (i.e. cores) to use in parallel operation')
-    parser.add_argument('-ls', '--largescalemode',
-                        type=bool,
-                        default=False,
-                        help='Omit detailed output data (generated input timeseries, system graphs, '
-                             'result timeseries, and timeseries plots)')
-    parser.add_argument('-db', '--debugmode',
-                        type=bool,
-                        default=False,
-                        help='Generate debug output and dump .lp model file for external solving')
-    parser.add_argument('-rer', '--rerun',
-                        type=str,
-                        default=False,
-                        help='Directory name of run including failed scenarios which should be rerun')
-    parser.add_argument('-rin', '--rerun_infeasible',
-                        type=str,
-                        default=True,
-                        help='Rerun infeasible or unbounded scenarios')
-    parser.add_argument('-ksc', '--key_solcast_api',
-                        type=str,
-                        default=None,
-                        help='API key for Solcast API')
+    parser.add_argument(
+        "-scn",
+        "--scenario",
+        type=str,
+        default=None,
+        help="Path to the scenario CSV file",
+    )
+    parser.add_argument(
+        "-in",
+        "--inputdir",
+        type=str,
+        default=None,
+        help="Path to the input data directory",
+    )
+    parser.add_argument(
+        "-out",
+        "--outputdir",
+        type=str,
+        default=None,
+        help="Path to the results directory",
+    )
+    parser.add_argument(
+        "-slv",
+        "--solver",
+        type=str,
+        default="gurobi",
+        help="Pyomo compatible solver to be used for the optimization problem.",
+    )
+    parser.add_argument(
+        "-np",
+        "--n_processes",
+        type=int,
+        default=1,
+        help="Number of processes (i.e. cores) to use in parallel operation",
+    )
+    parser.add_argument(
+        "-ls",
+        "--largescalemode",
+        type=bool,
+        default=False,
+        help="Omit detailed output data (generated input timeseries, system graphs, "
+        "result timeseries, and timeseries plots)",
+    )
+    parser.add_argument(
+        "-db",
+        "--debugmode",
+        type=bool,
+        default=False,
+        help="Generate debug output and dump .lp model file for external solving",
+    )
+    parser.add_argument(
+        "-rer",
+        "--rerun",
+        type=str,
+        default=False,
+        help="Directory name of run including failed scenarios which should be rerun",
+    )
+    parser.add_argument(
+        "-rin",
+        "--rerun_infeasible",
+        type=str,
+        default=True,
+        help="Rerun infeasible or unbounded scenarios",
+    )
+    parser.add_argument(
+        "-ksc",
+        "--key_solcast_api",
+        type=str,
+        default=None,
+        help="API key for Solcast API",
+    )
 
     args = parser.parse_args()
 
@@ -69,15 +108,21 @@ def main():
     # region interpret scenario file path
     # Option 1: No scenario file argument passed -> select via GUI
     if args.scenario is None:
+        if not TKINTER_AVAILABLE:
+            raise FileNotFoundError(
+                "No scenario file provided and tkinter is unavailable."
+            )
+
         root = tk.Tk()
         root.withdraw()  # hide small tk-window
         root.lift()  # make sure all tk windows appear in front of other windows
-        path_scenario = tk.filedialog.askopenfilename(initialdir=path_cwd,
-                                                      title=f'Select scenario file',
-                                                      filetypes=(('CSV files', '*.csv'),
-                                                                 ('All files', '*.*')))
+        path_scenario = tk.filedialog.askopenfilename(
+            initialdir=path_cwd,
+            title="Select scenario file",
+            filetypes=(("CSV files", "*.csv"), ("All files", "*.*")),
+        )
         if not path_scenario:
-            raise FileNotFoundError(f'No scenario file selected')
+            raise FileNotFoundError("No scenario file selected")
     # Option 2: Full absolute or relative file path (works from anywhere)
     elif os.path.isfile(args.scenario):
         path_scenario = args.scenario
@@ -85,13 +130,18 @@ def main():
     elif os.path.isfile(os.path.join(path_cwd, args.scenario)):
         path_scenario = os.path.join(path_cwd, args.scenario)
     # Option 4: Example file in example project in package directory (works from anywhere)
-    elif args.scenario in ['example', 'ex']:
+    elif args.scenario in ["example", "ex"]:
         scenarios_example = True
-        path_scenario = os.path.join(path_pkg, 'example', 'scenarios_example.csv')
-        warnings.warn(f'Using example scenario file \"{args.scenario}\", data, and output directory from '
-                      f'REVOL-E-TION - disregard if this is intended', DefaultFileLocationWarning)
+        path_scenario = os.path.join(path_pkg, "example", "scenarios_example.csv")
+        warnings.warn(
+            f'Using example scenario file "{args.scenario}", data, and output directory from '
+            f"REVOL-E-TION - disregard if this is intended",
+            DefaultFileLocationWarning,
+        )
     else:
-        raise FileNotFoundError(f'Scenario file or path not interpretable: {args.scenario}')
+        raise FileNotFoundError(
+            f"Scenario file or path not interpretable: {args.scenario}"
+        )
     # endregion
 
     # region interpret input directory path
@@ -100,10 +150,15 @@ def main():
         path_input = os.path.dirname(path_scenario)
     # Option 2: No input directory argument passed -> select via GUI
     elif args.inputdir is None:
-        path_input = tk.filedialog.askdirectory(initialdir=path_cwd,
-                                                title='Select input data directory')
+        if not TKINTER_AVAILABLE:
+            raise FileNotFoundError(
+                "No input directory provided and tkinter is unavailable."
+            )
+        path_input = tk.filedialog.askdirectory(
+            initialdir=path_cwd, title="Select input data directory"
+        )
         if not path_input:
-            raise NotADirectoryError(f'No input data directory selected')
+            raise NotADirectoryError(f"No input data directory selected")
     # Option 3: Full absolute or relative file path (works from anywhere)
     elif os.path.isdir(args.inputdir):
         path_input = args.inputdir
@@ -111,19 +166,26 @@ def main():
     elif os.path.isdir(os.path.join(path_cwd, args.inputdir)):
         path_input = os.path.join(path_cwd, args.inputdir)
     else:
-        raise NotADirectoryError(f'Input directory path not interpretable: {args.inputdir}')
+        raise NotADirectoryError(
+            f"Input directory path not interpretable: {args.inputdir}"
+        )
     # endregion
 
     # region interpret output directory path
     # Option 1: Example file in example project in package directory (works from anywhere)
     if scenarios_example:
-        path_output = os.path.join(path_pkg, 'results')
+        path_output = os.path.join(path_pkg, "results")
     # Option 2: No output directory argument passed -> select via GUI
     elif args.outputdir is None:
-        path_output = tk.filedialog.askdirectory(initialdir=path_cwd,
-                                                 title='Select output data directory')
+        if not TKINTER_AVAILABLE:
+            raise FileNotFoundError(
+                "No output directory provided and tkinter is unavailable."
+            )
+        path_output = tk.filedialog.askdirectory(
+            initialdir=path_cwd, title="Select output data directory"
+        )
         if not path_output:
-            raise NotADirectoryError(f'No output data directory selected')
+            raise NotADirectoryError("No output data directory selected")
     # Option 3: Full absolute or relative file path (works from anywhere)
     elif os.path.isdir(args.outputdir):
         path_output = args.outputdir
@@ -131,20 +193,24 @@ def main():
     elif os.path.isdir(os.path.join(path_cwd, args.outputdir)):
         path_output = os.path.join(path_cwd, args.outputdir)
     else:
-        raise NotADirectoryError(f'Output directory path not interpretable: {args.outputdir}')
+        raise NotADirectoryError(
+            f"Output directory path not interpretable: {args.outputdir}"
+        )
     # endregion
 
-    sim.SimulationRun(path_scenarios=path_scenario,
-                      path_input=path_input,
-                      path_output=path_output,
-                      solver=args.solver,
-                      n_processes=args.n_processes,
-                      largescalemode=args.largescalemode,
-                      debugmode=args.debugmode,
-                      rerun=args.rerun,
-                      rerun_infeasible=args.rerun_infeasible,
-                      key_solcast_api=args.key_solcast_api)
+    sim.SimulationRun(
+        path_scenarios=path_scenario,
+        path_input=path_input,
+        path_output=path_output,
+        solver=args.solver,
+        n_processes=args.n_processes,
+        largescalemode=args.largescalemode,
+        debugmode=args.debugmode,
+        rerun=args.rerun,
+        rerun_infeasible=args.rerun_infeasible,
+        key_solcast_api=args.key_solcast_api,
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
