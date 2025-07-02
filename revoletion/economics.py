487ec9497d4625687b3ada534fc2d6e3c5f6e572
Brian Dietermann <brian.dietermann@tum.de>
2025-07-02 09:05:44 +0200

    minor changes in economics.py

--- revoletion/economics.py
+++ revoletion/economics.py
@@ -224,12 +224,12 @@ class EconomicAggregator(EconomicPointOfInterest):
                          block=block,
                          scenario=scenario)
 
-        self.totex = {'prj': 0,
-                      'dis': 0,
-                      'ann': 0}
-        self.value = {'prj': 0,
-                      'dis': 0,
-                      'ann': 0}
+        self.totex = {'prj': 0.0,
+                      'dis': 0.0,
+                      'ann': 0.0}
+        self.value = {'prj': 0.0,
+                      'dis': 0.0,
+                      'ann': 0.0}
 
     def pre_scenario(self):
         if self.block is not None:
@@ -269,18 +269,18 @@ class EconomicEvaluator(EconomicPointOfInterest):
                          block=block)
 
         # region set default values
-        self.capex.update({'spec': 0,
-                           'fix': 0,})
-        self.mntex.update({'spec': 0,
-                           'fix': 0})
-        self.opex.update({'spec': transform_scalar_var(value=0,
+        self.capex.update({'spec': 0.0,
+                           'fix': 0.0,})
+        self.mntex.update({'spec': 0.0,
+                           'fix': 0.0})
+        self.opex.update({'spec': transform_scalar_var(value=0.0,
                                                        scenario=self.scenario,
                                                        block=block)})
-        self.crev.update({'spec': transform_scalar_var(value=0,
+        self.crev.update({'spec': transform_scalar_var(value=0.0,
                                                        scenario=self.scenario,
                                                        block=block)})
         self.aux = {'ls': self.scenario.prj_duration_yrs,
-                    'ccr': 1}
+                    'ccr': 1.0}
         self.size_name = None
         self.flow_name = None
         # endregion
@@ -293,11 +293,11 @@ class EconomicEvaluator(EconomicPointOfInterest):
             elif param_tuple == ('flow', 'name'):
                 self.flow_name = param_name
             elif dict_name in ['opex', 'crev']:
-                getattr(self, dict_name)[dict_key] = transform_scalar_var(value=getattr(self.block, param_name, 0),
+                getattr(self, dict_name)[dict_key] = transform_scalar_var(value=getattr(self.block, param_name, 0.0),
                                                                           scenario=self.scenario,
                                                                           block=self.block)
             else:  # capex, mntex, aux
-                getattr(self, dict_name)[dict_key] = getattr(self.block, param_name, 0)
+                getattr(self, dict_name)[dict_key] = getattr(self.block, param_name, 0.0)
         # endregion
 
         self.pre_scenario()
