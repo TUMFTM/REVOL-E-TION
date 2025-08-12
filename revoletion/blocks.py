09c4cf091bf15a55bd206e5a0ec72232ed89207d
Florian Honeder <florian.honeder@tum.de>
2025-08-12 10:35:34 -0600

    loss rate added to puffer of hp

--- revoletion/blocks.py
+++ revoletion/blocks.py
@@ -2585,7 +2585,7 @@ class Heatpump(SinkBlock):
             inputs={self.components['bus']: solph.Flow()},
             outputs={self.components['heating_bus']: solph.Flow()},
             initial_storage_level= 0.5,
-            loss_rate=0,
+            loss_rate=(self.lr_24h*3600/(self.size_buffer*self.specific_heat_capacity_h2o*35))/24,
             nominal_storage_capacity= (self.size_buffer * self.specific_heat_capacity_h2o * 35) / 3600
         )
 
