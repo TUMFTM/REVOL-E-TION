7065c6d504dd5aa3bd94a75880d5a07482c6a339
Florian Honeder <florian.honeder@tum.de>
2025-08-28 13:41:45 -0600

    add temp of buffer to states and hence add hovertamplate for buffer

--- revoletion/blocks.py
+++ revoletion/blocks.py
@@ -2511,6 +2511,7 @@ t_hot_dhw = 55
 t_cold_dhw = 10
 specific_heat_capacity_h2o = 4180
 t_delta_buffer = 20
+t_flow_temperature = 35
 class ThermalDemand(ThermalBlock):
 
     def init_evaluators(self):
@@ -2774,7 +2775,7 @@ class Heatpump(SinkBlock):
     def init_states(self):
         super().init_states()
 
-        for state in ['energy_buffer', 'soc_buffer']:
+        for state in ['energy_buffer', 'soc_buffer', 'temp_buffer']:
             self.states[state] = np.nan
 
     def __init__(self,
@@ -2862,12 +2863,16 @@ class Heatpump(SinkBlock):
 
         self.states.loc[horizon.dti_ch_extd, 'soc_buffer'] = (self.states.loc[horizon.dti_ch_extd, 'energy_buffer'] /
                                                               self.capacity_buffer).fillna(0)
+        self.states.loc[horizon.dti_ch_extd, 'temp_buffer'] = (
+                t_flow_temperature + self.states.loc[horizon.dti_ch_extd, 'soc_buffer'] * t_delta_buffer
+        )
 
     def create_plot_traces(self):
         super().create_plot_traces()
 
         soc_buffer = self.states.loc[utils.extend_dti(dti=self.scenario.dti_eval,
                                                       freq=self.scenario.timestep_td), 'soc_buffer'].dropna()
+        temp_buffer = self.states.loc[soc_buffer.index, 'temp_buffer']
 
         self.scenario.plot_traces.extend(
             plot_lines=[go.Scatter(x=self.scenario.dti_eval,
@@ -2881,7 +2886,11 @@ class Heatpump(SinkBlock):
                                    mode='lines',
                                    name=f'{self.name} buffer',
                                    line=dict(width=2, dash=None),
-                                   ),  # ToDo: add temperature of buffer for mouseover using argument "hovertemplate"
+                                   hovertemplate=(
+                                           'Temp: %{customdata:.1f}°C<extra></extra>'
+                                   ),
+                                   customdata=temp_buffer.values
+                                   ),
                         ],
             secondary_ys=[False, True]
         )
