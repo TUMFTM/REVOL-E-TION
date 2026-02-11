874d79853fb2449e28d534622a02b34537af8c43
Brian Dietermann <brian.dietermann@tum.de>
2026-02-11 09:02:03 +0100

    refactor: time

--- revoletion/time.py
+++ revoletion/time.py
@@ -55,13 +55,20 @@ class RunTimer:
         self.stop()
 
 
-@dataclass
+@dataclass(slots=True, frozen=True)
 class Timestep:
     td: pd.Timedelta
+    _hours: float
+
+    def __init__(self, td: pd.Timedelta):
+        # use custom init to enforce consistency between td and hours
+        # cached_property collides with slots=True -> store hours as a regular attribute
+        object.__setattr__(self, "td", td)
+        object.__setattr__(self, "_hours", td.total_seconds() / 3600)
 
     @property
     def hours(self) -> float:
-        return self.td.total_seconds() / 3600
+        return self._hours
 
     @classmethod
     def from_dti(cls, dti: pd.DatetimeIndex) -> Self:
@@ -84,7 +91,7 @@ class TimeFrame:
     def create_from_start_timestamp(
         cls,
         start: pd.Timestamp,
-        timestep: pd.Timedelta,
+        timestep: pd.Timedelta | str,
         end: pd.Timestamp | None = None,
         duration: pd.Timedelta | None = None,
     ) -> Self:
@@ -97,15 +104,15 @@ class TimeFrame:
         # always recalculate end to ensure consistency
         end = start + duration
 
-        return cls(start=start, end=end, duration=duration, _timestep=timestep)
+        return cls(start=start, end=end, duration=duration, _timestep=pd.Timedelta(timestep))
 
     @cached_property
     def dti(self) -> pd.DatetimeIndex:
-        return pd.date_range(start=self.start, end=self.end, freq=self._timestep, inclusive="left")
+        return pd.date_range(start=self.start, end=self.end, freq=self._timestep, inclusive="left", name="time")
 
     @cached_property
     def dti_extd(self) -> pd.DatetimeIndex:
-        return pd.date_range(start=self.start, end=self.end, freq=self._timestep, inclusive="both")
+        return pd.date_range(start=self.start, end=self.end, freq=self._timestep, inclusive="both", name="time")
 
     @cached_property
     def end_extd(self) -> pd.Timestamp:
@@ -121,51 +128,58 @@ class SimulationTimes:
     @classmethod
     def create_from_plain(
         cls,
-        timestep: str,
-        timezone: pytz.BaseTzInfo,
-        starttime: str,
-        sim_endtime: str,
-        sim_duration: str,
-        prj_duration: str,
+        timestep: str | pd.Timedelta | Timestep,
+        timezone: pytz.BaseTzInfo | str,
+        starttime: str | pd.Timestamp,
+        sim_endtime: str | pd.Timestamp | None,
+        sim_duration: float | int | None,
+        prj_duration: int,
     ) -> Self:
-        starttime_timestamp = cls._convert_time_str(starttime, timestep, timezone)
-        if starttime_timestamp is None:
-            raise ValueError(f"Failed to convert starttime ({starttime}) to pd.Timestamp")
-
-        sim_endtime_timestamp = cls._convert_time_str(sim_endtime, timestep, timezone)
-
-        timestep_timedelta = pd.Timedelta(timestep)
-        if timestep_timedelta is None:
+        timestep_td = pd.Timedelta(timestep) if not isinstance(timestep, Timestep) else timestep.td
+        if timestep_td is None:
             raise ValueError(f"Failed to convert timestep '{timestep}' to pd.Timedelta")
 
-        sim_duration_timedelta = pd.Timedelta(sim_duration, unit="day") if sim_duration is not None else None
+        starttime_ts = cls._convert_time_str(starttime, timestep_td, timezone)
+        if starttime_ts is None:
+            raise ValueError(f"Failed to convert starttime ({starttime}) to pd.Timestamp")
 
-        sim = TimeFrame.create_from_start_timestamp(
-            start=starttime_timestamp,
-            timestep=timestep_timedelta,
-            end=sim_endtime_timestamp,
-            duration=sim_duration_timedelta,
+        sim_endtime_ts = cls._convert_time_str(sim_endtime, timestep, timezone) if sim_endtime is not None else None
+        sim_duration_td = pd.Timedelta(sim_duration, unit="day") if sim_duration is not None else None
+
+        simulation = TimeFrame.create_from_start_timestamp(
+            start=starttime_ts,
+            timestep=timestep_td,
+            end=sim_endtime_ts,
+            duration=sim_duration_td,
         )
-        eval = TimeFrame.create_from_start_timestamp(
-            start=starttime_timestamp,
-            timestep=timestep_timedelta,
-            end=sim_endtime_timestamp,
-            duration=sim_duration_timedelta,
+        evaluation = TimeFrame.create_from_start_timestamp(
+            start=starttime_ts,
+            timestep=timestep_td,
+            end=sim_endtime_ts,
+            duration=sim_duration_td,
         )
-        prj = TimeFrame.create_from_start_timestamp(
-            start=starttime_timestamp,
-            timestep=timestep_timedelta,
-            end=starttime_timestamp + pd.DateOffset(years=prj_duration),
+        project = TimeFrame.create_from_start_timestamp(
+            start=starttime_ts,
+            timestep=timestep_td,
+            end=starttime_ts + pd.DateOffset(years=prj_duration),
         )
 
-        return cls(sim=sim, eval=eval, prj=prj)
+        return cls(sim=simulation, eval=evaluation, prj=project)
 
     @staticmethod
-    def _convert_time_str(time_str: str | None, timestep: str, timezone: pytz.BaseTzInfo) -> pd.Timestamp | None:
-        if time_str is None:
+    def _convert_time_str(
+        time_in: str | pd.Timestamp | None, timestep: pd.Timedelta | str, timezone: pytz.BaseTzInfo | str
+    ) -> pd.Timestamp | None:
+        if time_in is None:
             return None
 
+        if isinstance(time_in, pd.Timestamp):
+            if time_in.tz is None:
+                return time_in.tz_localize(timezone)
+            else:
+                return time_in.tz_convert(timezone)
+
         # ToDo: reformat time
-        time_str = time_str if len(time_str) > 10 else time_str + " 00:00"
-        value = pd.to_datetime(time_str, format="%d.%m.%Y %H:%M").floor(timestep).tz_localize(timezone)
+        time_in = time_in if len(time_in) > 10 else time_in + " 00:00"
+        value = pd.to_datetime(time_in, format="%d.%m.%Y %H:%M").floor(timestep).tz_localize(timezone)
         return value
