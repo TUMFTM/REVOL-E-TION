fix(rl/normalization): Wrong normalzation constants for output Opex

revoletion/optimization/pypsa_backend/_pypsa_optimization_problem.py
@@ -369,7 +369,7 @@ class PypsaOptimizationProblem(optimization_problem.OptimizationProblem):
         self._net = net
 
         if self._config.warmstart:
-            self._model = self._net.optimize.create_model()
+            self._model = self._net.optimize.create_model(include_objective_constant=False)
             self._warmstart_folder = tempfile.TemporaryDirectory()
         else:
             self._model = None
@@ -450,6 +450,7 @@ class PypsaOptimizationProblem(optimization_problem.OptimizationProblem):
 
         normalized_dti = pypsa_utils.normalize_dti_or_df(dti)
         charger_in = make_pypsa_label(block, "inflow-link")
+
         self._net.c.links.dynamic.p_min_pu.loc[normalized_dti, charger_in] = power_unit
         # Set the fixed flow to NaN to avoid any numerical issues with the solver and let PyPSA figure out the exact flow.
         self._net.c.links.dynamic.p_set.loc[normalized_dti, charger_in] = np.nan
@@ -500,6 +501,18 @@ class PypsaOptimizationProblem(optimization_problem.OptimizationProblem):
         normalized_dti = pypsa_utils.normalize_dti_or_df(dti)
         charger_out = make_pypsa_label(block, "outflow-link")
         self._net.c.links.dynamic.p_min_pu.loc[normalized_dti, charger_out] = power_unit
+        # Set the fixed flow to NaN to avoid any numerical issues with the solver and let PyPSA figure out the exact flow.
+        self._net.c.links.dynamic.p_set.loc[normalized_dti, charger_out] = np.nan
+
+    def set_maximum_output_power_unit(
+        self, block: blocks.ElectricBlock, power_unit: float, dti: pd.DatetimeIndex
+    ) -> None:
+        if not isinstance(block, blocks.ElectricFleetUnit):
+            raise ValueError(f"Cannot set maximum output power for block {block.name} of type {type(block)}")
+
+        normalized_dti = pypsa_utils.normalize_dti_or_df(dti)
+        charger_out = make_pypsa_label(block, "outflow-link")
+        self._net.c.links.dynamic.p_max_pu.loc[normalized_dti, charger_out] = power_unit
         # Set the fixed flow to NaN to avoid any numerical issues with the solver and let PyPSA figure out the exact flow.
         self._net.c.links.dynamic.p_set.loc[normalized_dti, charger_out] = np.nan
 

revoletion/rl/environment.py
@@ -14,7 +14,7 @@ from revoletion.rl.scenario_factory import (
     AtBaseHorizonInitializer,
     HorizonInitializer,
     InitialSocHorizonInitializer,
-    SocEnvelopeHorizonInitialzer,
+    MinSocHorizonInitializer,
 )
 
 from . import _context as context
@@ -28,18 +28,18 @@ _LOGGER = logging.getLogger(__name__)
 
 @dataclass
 class RewardConfig:
-    penalty_factor_grid_opex: float = -10.0
+    penalty_factor_grid_opex: float = 10.0
     """Factor applied to the costs of importing/exporting energy to the grid."""
 
-    penalty_factor_charge_opex: float = -0.0
+    penalty_factor_charge_opex: float = 0.0
     """Weight applied to the costs of charging/discharging the EVs."""
 
-    penalty_factor_gen_opex: float = -1.0
+    penalty_factor_gen_opex: float = 1.0
     """Weight applied to the costs of charging/discharging the EVs."""
 
-    penalty_factor_ext_charge_opex: float = -0.0
+    penalty_factor_ext_charge_opex: float = 0.0
 
-    penalty_base_dsoc: float = -5.0
+    penalty_base_dsoc: float = -10.0
 
     penalty_factor_dsoc: float = 10.0
     """Weight for the penalty if the agent does not met the SoC requirements."""
@@ -58,12 +58,12 @@ class RewardConfig:
 
     penalty_base_power_diff: float = -0.05
 
-    penalty_factor_power_diff: float = -0.05
+    penalty_factor_power_diff: float = 0.05
     """Weight for the penalty if the agent tries to charge with a power that would exceed the maximal/minimum capacity of an EV."""
 
     penalty_base_atbase_violation: float = -0.05
 
-    penalty_factor_atbase_violation: float = -0.05
+    penalty_factor_atbase_violation: float = 0.05
     """Weight for the penalty if the agent tries to charge an EV even though the EV is currently not available at the charger."""
 
     reward_factor_step: float = 0.0
@@ -110,22 +110,22 @@ class RewardComponents:
     @property
     def grid_opex_reward(self) -> float:
         if self.grid_opex > 0.0:
-            return self.grid_opex * self.config.penalty_factor_grid_opex
-        return self.grid_opex
+            return -self.grid_opex * self.config.penalty_factor_grid_opex
+        return -self.grid_opex
 
     @property
     def charge_opex_reward(self) -> float:
-        return self.charge_opex * self.config.penalty_factor_charge_opex
+        return -self.charge_opex * self.config.penalty_factor_charge_opex
 
     @property
     def ext_charge_opex_reward(self) -> float:
-        return self.ext_charge_opex * self.config.penalty_factor_ext_charge_opex
+        return -self.ext_charge_opex * self.config.penalty_factor_ext_charge_opex
 
     @property
     def gen_opex_reward(self) -> float:
         if self.gen_opex > 0.0:
-            return self.gen_opex * self.config.penalty_factor_gen_opex
-        return self.gen_opex
+            return -self.gen_opex * self.config.penalty_factor_gen_opex
+        return -self.gen_opex
 
     @property
     def power_diff_reward(self) -> float:
@@ -136,7 +136,7 @@ class RewardComponents:
         power_diffs_base_penalty = self.config.penalty_base_power_diff * num_power_diffs
 
         power_diff_sum = sum([abs(power_diff) for power_diff in self.power_diffs])
-        scaled_power_diffs = power_diff_sum * self.config.penalty_factor_power_diff
+        scaled_power_diffs = -power_diff_sum * self.config.penalty_factor_power_diff
 
         return power_diffs_base_penalty + scaled_power_diffs
 
@@ -149,7 +149,7 @@ class RewardComponents:
         atbase_violations_base_penalty = self.config.penalty_base_atbase_violation * num_violations
 
         atbase_violations_sum = sum([abs(violation) for violation in self.atbase_violations])
-        scaled_atbase_violations = atbase_violations_sum * self.config.penalty_factor_atbase_violation
+        scaled_atbase_violations = -atbase_violations_sum * self.config.penalty_factor_atbase_violation
 
         return atbase_violations_base_penalty + scaled_atbase_violations
 
@@ -252,7 +252,7 @@ class RevoletionEnvironment(gym.Env[ObsType, ActType]):
 
         self._horizon_initializer = HorizonInitializer(
             horizon_initializers=[
-                SocEnvelopeHorizonInitialzer(
+                MinSocHorizonInitializer(
                     soc_min=self._config.soc_min,
                 ),
                 InitialSocHorizonInitializer(rng=np.random.default_rng(42)),

revoletion/rl/imitation_learning.py
@@ -31,7 +31,7 @@ from revoletion.rl.scenario_factory import (
     AtBaseHorizonInitializer,
     HorizonInitializer,
     InitialSocHorizonInitializer,
-    SocEnvelopeHorizonInitialzer,
+    MinSocHorizonInitializer,
 )
 
 from . import _context as context
@@ -90,11 +90,12 @@ class ImitationTrajectoryComputer:
         self.logger = logger or logging.getLogger(__name__)
         self._horizon_initializer = HorizonInitializer(
             horizon_initializers=[
-                SocEnvelopeHorizonInitialzer(
-                    soc_min=self._config.soc_min + self._config.envelope_soc_padding,
-                    max_charge_power_frac=self._config.envelope_max_charge_buffer,
-                    envelope_target_soc=self._config.envelope_target_soc,
-                ),
+                MinSocHorizonInitializer(soc_min=self._config.soc_min),
+                # SocEnvelopeHorizonInitialzer(
+                #     soc_min=self._config.soc_min + self._config.envelope_soc_padding,
+                #     max_charge_power_frac=self._config.envelope_max_charge_buffer,
+                #     envelope_target_soc=self._config.envelope_target_soc,
+                # ),
                 InitialSocHorizonInitializer(),
                 AtBaseHorizonInitializer(),
             ]
@@ -307,12 +308,8 @@ class ImitationTrajectoryComputer:
             for time_step in episode_horizon.dti:
                 target_power_unit = power_envelope[time_step]
                 if target_power_unit > 0.0:
-                    problem.set_input_power_unit(
-                        block,
-                        target_power_unit,
-                        time_step,
-                        power_unit_buffer=self._config.envelope_power_unit_buffer,
-                    )
+                    problem.set_maximum_output_power_unit(block, power_unit=0.0, dti=time_step)
+                    problem.set_minimum_input_power_unit(block, target_power_unit, time_step)
                 else:
                     problem.set_input_power_unit(
                         block,
@@ -340,11 +337,19 @@ class ImitationTrajectoryComputer:
             Array of target power unit charge values
         """
         # Extract block parameters
+        nom_capacity_wh = block.sizes["storage"].preexisting
         eff_charge = block.eff["chg_int"]
         max_charge_power_w = block.pwr_chg_max * eff_charge
+        dsoc_step_max = (
+            max_charge_power_w * self._config.envelope_max_charge_buffer * episode_horizon.timestep.hours
+        ) / nom_capacity_wh
 
-        soc_envelope = block.states.loc[episode_horizon.dti, "soc_min"]
-        power_envelope = rl_utils.get_power_envelope(block, episode_horizon, soc_envelope)
+        soc_envelope = rl_utils.get_soc_envelope(block, episode_horizon, dsoc_step_max=dsoc_step_max)
+
+        soc_min = block.states.loc[episode_horizon.dti, "soc_min"]
+        buffered_soc_envelope = soc_envelope + soc_min
+
+        power_envelope = rl_utils.get_power_envelope(block, episode_horizon, buffered_soc_envelope)
 
         target_power_unit = np.clip(power_envelope / max_charge_power_w, 0.0, 1.0)
         return target_power_unit
@@ -367,11 +372,11 @@ class ImitationTrajectoryComputer:
             Tuple of (observations_buffer, actions_buffer)
         """
         perfect_forecast_provider = forecast_provider.PerfectForesightForecastProvider(self._config.forecast_horizon)
-        limited_forecast_provider = forecast_provider.LimitedForecastProvider(self._config.forecast_horizon)
+        # limited_forecast_provider = forecast_provider.LimitedForecastProvider(self._config.forecast_horizon)
 
         normalization_provider = normalization.NormalizationProvider.from_ctx(ctx)
         feature_extractor = features.EnvironmentFeatureExtractor(
-            limited_forecast_provider, soc_min=self._config.soc_min, normalization_provider=normalization_provider
+            perfect_forecast_provider, soc_min=self._config.soc_min, normalization_provider=normalization_provider
         )
 
         observations_buffer = []

revoletion/rl/scenario_factory.py
@@ -19,11 +19,24 @@ class HorizonInitializerInterface(abc.ABC):
     def initialze_scenario_for_horizon(self, scenario: scn.Scenario, horizon: utils.TimeSettings) -> None: ...
 
 
-class SocEnvelopeHorizonInitialzer(HorizonInitializerInterface):
-    def __init__(
-        self, soc_min: float = 0.0, max_charge_power_frac: float = 1.0, envelope_target_soc: float = 0.0
-    ) -> None:
+class MinSocHorizonInitializer(HorizonInitializerInterface):
+    def __init__(self, soc_min: float = 0.0) -> None:
         self._soc_min = soc_min
+
+    def initialze_scenario_for_horizon(self, scenario: scn.Scenario, horizon: utils.TimeSettings) -> None:
+        for electric_fleet_unit_block in scenario.block_registry.get("ElectricFleetUnit", {}).values():
+            prev_soc_min = electric_fleet_unit_block.states.loc[horizon.dti, "soc_min"]
+            new_soc_min = np.clip(prev_soc_min + self._soc_min, 0.0, 1.0)
+            electric_fleet_unit_block.states.loc[horizon.dti, "soc_min"] = new_soc_min
+
+        for stationary_battery_block in scenario.block_registry.get("StationaryBattery", {}).values():
+            prev_soc_min = electric_fleet_unit_block.states.loc[horizon.dti, "soc_min"]
+            new_soc_min = np.clip(prev_soc_min + self._soc_min, 0.0, 1.0)
+            stationary_battery_block.states.loc[horizon.dti, "soc_min"] = new_soc_min
+
+
+class SocEnvelopeHorizonInitialzer(HorizonInitializerInterface):
+    def __init__(self, max_charge_power_frac: float = 1.0, envelope_target_soc: float = 0.0) -> None:
         self._max_charge_power_frac = max_charge_power_frac
         self._envelope_target_soc = envelope_target_soc
 
@@ -35,21 +48,18 @@ class SocEnvelopeHorizonInitialzer(HorizonInitializerInterface):
             max_charge_power_w = electric_fleet_unit_block.pwr_chg_max * eff_charge * eff_storage_roundtrip
 
             buffered_max_charge_power_w = max_charge_power_w * self._max_charge_power_frac
-
             # Determine a smoothed out dsoc step that is used for the SoC envelope computation.
             # This gives the optimizer a bit more freedom and reduces the number of infeasibilities.
-            dsoc_step = (buffered_max_charge_power_w * horizon.timestep.hours) / nom_capacity_wh
+            dsoc_step_max = (buffered_max_charge_power_w * horizon.timestep.hours) / nom_capacity_wh
 
             soc_envelope = rl_utils.get_soc_envelope(
-                electric_fleet_unit_block, horizon, dsoc_step=dsoc_step, target_soc=self._envelope_target_soc
+                electric_fleet_unit_block, horizon, dsoc_step_max=dsoc_step_max, target_soc=self._envelope_target_soc
             )
+            soc_min = electric_fleet_unit_block.states.loc[horizon.dti, "soc_min"]
 
-            buffered_soc_envelope = np.clip(soc_envelope + self._soc_min, 0.0, 1.0)
+            buffered_soc_envelope = np.clip(soc_envelope + soc_min, 0.0, 1.0)
 
             electric_fleet_unit_block.states.loc[horizon.dti, "soc_min"] = buffered_soc_envelope
-
-        for stationary_battery_blocks in scenario.block_registry.get("StationaryBattery", {}).values():
-            stationary_battery_blocks.states.loc[horizon.dti, "soc_min"] = self._soc_min
 
 
 class AtBaseHorizonInitializer(HorizonInitializerInterface):
@@ -68,14 +78,18 @@ class InitialSocHorizonInitializer(HorizonInitializerInterface):
 
     def initialze_scenario_for_horizon(self, scenario: scn.Scenario, horizon: utils.TimeSettings) -> None:
         for electric_fleet_unit_block in scenario.block_registry.get("ElectricFleetUnit", {}).values():
-            initial_soc_min = electric_fleet_unit_block.states.loc[horizon.start, "soc_min"]
+            soc_envelope = rl_utils.get_soc_envelope(electric_fleet_unit_block, horizon)
+            soc_min = electric_fleet_unit_block.states.loc[horizon.start, "soc_min"]
+            buffered_soc_envelope = np.clip(soc_envelope + soc_min, 0.0, 1.0)
+
+            initial_soc_min = buffered_soc_envelope.loc[horizon.start]
 
             initial_soc = self._rng.uniform(initial_soc_min, 1.0)
             electric_fleet_unit_block.states.loc[horizon.start, "soc"] = initial_soc
 
         for stationary_battery_blocks in scenario.block_registry.get("StationaryBattery", {}).values():
-            initial_soc_min = electric_fleet_unit_block.states.loc[horizon.start, "soc_min"]
-            initial_soc = self._rng.uniform(low=initial_soc_min, high=1.0)
+            soc_min = electric_fleet_unit_block.states.loc[horizon.start, "soc_min"]
+            initial_soc = self._rng.uniform(low=soc_min, high=1.0)
             stationary_battery_blocks.states.loc[horizon.start, "soc"] = initial_soc
 
 

revoletion/rl/utils.py
@@ -1,4 +1,3 @@
-from mpmath.functions.functions import re
 import numpy as np
 import pandas as pd
 
@@ -8,7 +7,8 @@ from revoletion import blocks, utils
 def get_soc_envelope(
     block: blocks.ElectricFleetUnit,
     horizon: utils.TimeSettings,
-    dsoc_step: float | None = None,
+    dsoc_step_max: float | None = None,
+    dsoc_step_min: float | None = None,
     target_soc: float | None = None,
 ) -> pd.Series:
     dti = horizon.dti
@@ -18,10 +18,15 @@ def get_soc_envelope(
     nom_capacity_wh = block.sizes["storage"].preexisting
 
     max_charge_power_w = block.pwr_chg_max * block.eff["chg_int"] * np.sqrt(block.eff["storage_roundtrip"])
-    if dsoc_step is None:
+    if dsoc_step_max is None:
         dsoc_step_max = (max_charge_power_w * horizon.timestep.hours) / nom_capacity_wh
     else:
-        dsoc_step_max = dsoc_step
+        dsoc_step_max = dsoc_step_max
+
+    if dsoc_step_min is None:
+        dsoc_step_min = (max_charge_power_w * 0.1 * horizon.timestep.hours) / nom_capacity_wh
+    else:
+        dsoc_step_min = dsoc_step_min
 
     consumption = block.log.loc[dti, "consumption"] * horizon.timestep.hours
     # Need at least enough SoC to compensate standing loss.
@@ -37,7 +42,8 @@ def get_soc_envelope(
         required_soc = min(required_soc + dsoc[time_step], 1.0)
 
         if plugged[time_step]:
-            required_soc = max(required_soc - dsoc_step_max, 0.0)
+            if required_soc > dsoc_step_min:
+                required_soc = max(required_soc - dsoc_step_max, 0.0)
 
         soc_floor[time_step] = required_soc
 
@@ -61,6 +67,8 @@ def get_power_envelope(
     current_soc = block.states.loc[horizon.start, "soc"]
     nom_capacity_wh = block.sizes["storage"].preexisting
     timestep_hours = horizon.timestep.hours
+    max_charge_power_w = block.pwr_chg_max * block.eff["chg_int"] * np.sqrt(block.eff["storage_roundtrip"])
+    dsoc_step_max = (max_charge_power_w * horizon.timestep.hours) / nom_capacity_wh
 
     consumption = block.log.loc[horizon.dti, "consumption"] * horizon.timestep.hours
     # Need at least enough SoC to compensate standing loss.
@@ -76,21 +84,16 @@ def get_power_envelope(
 
         if plugged[current_time_step]:
             required_dsoc = max(soc_envelope[next_time_step] - current_soc, 0.0)
-            current_soc += required_dsoc
+            charge_dsoc = min(required_dsoc, dsoc_step_max)
+            power_required = (
+                (charge_dsoc * nom_capacity_wh)
+                / timestep_hours
+                / block.eff["chg_int"]
+                / np.sqrt(block.eff["storage_roundtrip"])
+            )
 
-            if required_dsoc > 0.0:
-                # Convert to power (W)
-                # Positive dsoc means SoC needs to increase (charging)
-                charge_dsoc = min(required_dsoc, 1.0)
-                power_required = (
-                    (charge_dsoc * nom_capacity_wh)
-                    / timestep_hours
-                    / block.eff["chg_int"]
-                    / np.sqrt(block.eff["storage_roundtrip"])
-                )
-
+            if power_required > 0.0:
+                current_soc += required_dsoc
                 power_envelope[current_time_step] = power_required
-
-        print(current_time_step, current_soc, soc_envelope[current_time_step], power_envelope[current_time_step])
 
     return power_envelope

revoletion/simulation.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-from revoletion.scheduler import AprioriPowerScheduler
 import logging
 import types
 from dataclasses import dataclass
@@ -13,12 +12,12 @@ from revoletion.rl.scenario_factory import (
     AtBaseHorizonInitializer,
     HorizonInitializer,
     InitialSocHorizonInitializer,
-    SocEnvelopeHorizonInitialzer,
+    MinSocHorizonInitializer,
 )
 
 from . import blocks, optimization, rl, utils
-from .rl import utils as rl_utils
 from . import scenario as scn
+from .rl import utils as rl_utils
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -527,9 +526,8 @@ class ControlHorizon:
 
         horizon_initializer = HorizonInitializer(
             horizon_initializers=[
-                SocEnvelopeHorizonInitialzer(
+                MinSocHorizonInitializer(
                     soc_min=0.05,
-                    max_charge_power_frac=0.95,
                 ),
                 InitialSocHorizonInitializer(rng=np.random.default_rng(42)),
                 AtBaseHorizonInitializer(),
@@ -546,6 +544,7 @@ class ControlHorizon:
         )
         for block in scenario.block_registry.get("ElectricFleetUnit", {}).values():
             optimization_problem.set_minimum_input_power_unit(block, 0.1, eval_horizon.dti)
+            optimization_problem.set_minimum_output_power_unit(block, 0.1, eval_horizon.dti)
 
         _, optimization_result = optimization_problem.solve()
         return optimization_result
@@ -558,14 +557,12 @@ class ControlHorizon:
             solver=optimization.Solver.HIGHS,
             enforce_soc_constraints=True,
             invest=False,
-            committment=False,
         )
 
         horizon_initializer = HorizonInitializer(
             horizon_initializers=[
-                SocEnvelopeHorizonInitialzer(
+                MinSocHorizonInitializer(
                     soc_min=0.05,
-                    max_charge_power_frac=0.95,
                 ),
                 InitialSocHorizonInitializer(rng=np.random.default_rng(42)),
                 AtBaseHorizonInitializer(),
@@ -573,9 +570,6 @@ class ControlHorizon:
         )
 
         horizon_initializer.initialize(scenario, eval_horizon)
-
-        scheduler = AprioriPowerScheduler(scenario)
-        scheduler.calc_ph_schedule(eval_horizon)
 
         optimization_problem = optimization.PypsaOptimizationProblem.from_revoletion_scenario(
             scenario,
@@ -588,16 +582,19 @@ class ControlHorizon:
             eff_charge = block.eff["chg_int"]
             max_charge_power_w = block.pwr_chg_max * eff_charge
 
-            soc_envelope = block.states.loc[eval_horizon.dti, "soc_min"]
-            power_envelope = rl_utils.get_power_envelope(block, eval_horizon, soc_envelope)
+            prev_soc_min = block.states.loc[eval_horizon.dti, "soc_min"]
+            soc_envelope = rl_utils.get_soc_envelope(block, eval_horizon)
+            buffered_soc_envelope = np.clip(soc_envelope + prev_soc_min, 0.0, 1.0)
+            power_envelope = rl_utils.get_power_envelope(block, eval_horizon, buffered_soc_envelope)
 
-            target_power_unit = np.clip(power_envelope / max_charge_power_w, 0.0, 1.0)
+            power_frac_envelope = np.clip((power_envelope / max_charge_power_w), 0.0, 1.0)
             for time_step in eval_horizon.dti:
-                power_unit_buffer = 1e-2 if target_power_unit[time_step] > 0.0 else 0.0
-                optimization_problem.set_input_power_unit(
-                    block, target_power_unit[time_step], time_step, power_unit_buffer=power_unit_buffer
-                )
-            block.states.loc[eval_horizon.dti, "soc_min"] = 0.05
+                target_power_unit = power_frac_envelope[time_step]
+                if target_power_unit > 0.0:
+                    optimization_problem.set_maximum_output_power_unit(block, power_unit=0.0, dti=time_step)
+                    optimization_problem.set_minimum_input_power_unit(block, target_power_unit, time_step)
+                else:
+                    optimization_problem.set_input_power_unit(block, power_unit=0.0, dti=time_step)
 
         _, optimization_result = optimization_problem.solve()
         return optimization_result